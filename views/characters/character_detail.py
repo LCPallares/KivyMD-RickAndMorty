c2684723981977e14075eee23026494bed44b247
LCPallares <poncho6257@gmail.com>
2025-06-22 19:42:57 -0500

    mejorado diseño de pantalla detalle de personaje

--- views/characters/character_detail.py
+++ views/characters/character_detail.py
@@ -4,12 +4,26 @@ from kivy.lang import Builder
 
 Builder.load_file('views/characters/character_detail.kv')
 
+# make in kv
+# from kivy.properties import StringProperty
+# from kivymd.uix.boxlayout import MDBoxLayout
+
+# class InfoLabel(MDBoxLayout):
+#     text1= StringProperty()
+#     text2 = StringProperty()
+
+
 class CharacterDetailScreen(MDScreen):
     character_id = NumericProperty(0)
     character_name = StringProperty('')
     character_status = StringProperty('')
     character_species = StringProperty('')
     character_image = StringProperty('')
+    
+    character_gender = StringProperty('')
+    character_origin = StringProperty('')
+    character_location = StringProperty('')
+    character_created = StringProperty('')
 
     def __init__(self, controller, **kwargs):
         self.controller = controller
@@ -24,5 +38,10 @@ class CharacterDetailScreen(MDScreen):
         self.character_status = character['status']
         self.character_species = character['species']
         self.character_image = character['image']
+        
+        self.character_gender = character['gender']
+        self.character_origin = character['origin']['name']
+        self.character_location = character['location']['name']
+        self.character_created = character['created']
 
 
